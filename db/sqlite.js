c25e2afbc4908863ee6f3ea898e1e1aa3a0f1164
Fuwn <contact@fuwn.me>
2024-01-19 03:15:10 -0800

    feat(db): move db to folder

--- db/sqlite.js
+++ db/sqlite.js
@@ -1,9 +1,9 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const Database = require('better-sqlite3')
+const path = require("path");
+const Database = require("better-sqlite3");
 
-const db = new Database(path.resolve(__dirname, '../count.db'))
+const db = new Database(path.resolve(__dirname, "../data/count.db"));
 
 db.exec(`CREATE TABLE IF NOT EXISTS tb_count (
     id    INTEGER      PRIMARY KEY AUTOINCREMENT
@@ -13,36 +13,38 @@ db.exec(`CREATE TABLE IF NOT EXISTS tb_count (
                        UNIQUE,
     num   BIGINT       NOT NULL
                        DEFAULT (0) 
-);`)
+);`);
 
 function getNum(name) {
   return new Promise((resolve, reject) => {
-    const stmt = db.prepare('SELECT `name`, `num` from tb_count WHERE `name` = ?')
-    const row = stmt.get(name)
-    resolve(row || { name, num: 0 })
-  })
+    const stmt = db.prepare(
+      "SELECT `name`, `num` from tb_count WHERE `name` = ?"
+    );
+    const row = stmt.get(name);
+    resolve(row || { name, num: 0 });
+  });
 }
 
 function getAll(name) {
   return new Promise((resolve, reject) => {
-    const stmt = db.prepare('SELECT * from tb_count')
-    const rows = stmt.all()
-    resolve(rows)
-  })
+    const stmt = db.prepare("SELECT * from tb_count");
+    const rows = stmt.all();
+    resolve(rows);
+  });
 }
 
 function setNum(name, num) {
   return new Promise((resolve, reject) => {
-    db.exec(`INSERT INTO tb_count(\`name\`, \`num\`)
+    db.exec(
+      `INSERT INTO tb_count(\`name\`, \`num\`)
             VALUES($name, $num)
             ON CONFLICT(name) DO
-            UPDATE SET \`num\` = $num;`
-      ,
+            UPDATE SET \`num\` = $num;`,
       { $name: name, $num: num }
-    )
+    );
 
-    resolve()
-  })
+    resolve();
+  });
 }
 
 function setNumMulti(counters) {
@@ -50,20 +52,20 @@ function setNumMulti(counters) {
     const stmt = db.prepare(`INSERT INTO tb_count(\`name\`, \`num\`)
     VALUES($name, $num)
     ON CONFLICT(name) DO
-    UPDATE SET \`num\` = $num;`)
+    UPDATE SET \`num\` = $num;`);
 
     const setMany = db.transaction((counters) => {
-      for (const counter of counters) stmt.run(counter)
-    })
+      for (const counter of counters) stmt.run(counter);
+    });
 
-    setMany(counters)
-    resolve()
-  })
+    setMany(counters);
+    resolve();
+  });
 }
 
 module.exports = {
   getNum,
   getAll,
   setNum,
-  setNumMulti
-}
+  setNumMulti,
+};
